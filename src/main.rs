bb976a65d4aab6f857c4abcb4edf54d816a55972
Aerton <aertoncastro@gmail.com>
2022-10-28 16:54:26 -0300

    duplicate bug

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use std::{thread, net::SocketAddr};
+use std::{thread, net::SocketAddr, collections::HashMap};
 
 use laminar::{Socket, Packet, SocketEvent};
 
 // Creates the socket
 fn main() {
-    let mut clients: Vec<SocketAddr> = Vec::new();
+    let mut clients: HashMap<String, SocketAddr> = HashMap::new();
     let mut socket = Socket::bind("0.0.0.0:4242").unwrap();
     let packet_sender = socket.get_packet_sender();
     let event_receiver = socket.get_event_receiver();
@@ -19,15 +19,16 @@ fn main() {
                 match socket_event {
                     SocketEvent::Packet(packet) => {
                         let endpoint = packet.addr();
-                        match (&clients).binary_search(&endpoint){
-                            Ok(_) => {},
-                            Err(_) => {
+                        let address = format!("{}:{}", endpoint.ip(), endpoint.port());
+                        match (&clients).get(&address){
+                            Some(_) => {},
+                            None => {
                                 println!("Player connected. port: {}", endpoint.port());
-                                (&mut clients).push(endpoint);
+                                (&mut clients).insert(address, endpoint);
                             }
                         }
                         let received_data = packet.payload();
-                        for client in &clients {
+                        for client in (&clients).values() {
                             if client.ip() != endpoint.ip() || client.port() != endpoint.port(){
                                 let unreliable = Packet::unreliable(*client, received_data.to_vec());
                                 packet_sender.send(unreliable).unwrap();
