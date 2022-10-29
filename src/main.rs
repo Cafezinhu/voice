1e4665f084b9bf0927fe67a46b706debbe3b53b4
Aerton <aertoncastro@gmail.com>
2022-10-29 15:57:49 -0300

    ten seconds timeout

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,45 @@
-use std::{thread, net::SocketAddr, collections::HashMap};
+use std::{thread, net::SocketAddr, collections::HashMap, time::{SystemTime, Duration}, sync::{Arc, Mutex}};
 
 use laminar::{Socket, Packet, SocketEvent};
 
+struct PlayerData{
+    addr: SocketAddr,
+    last_time_listened: SystemTime
+}
 // Creates the socket
 fn main() {
-    let mut clients: HashMap<String, SocketAddr> = HashMap::new();
+    let mut clients: Arc<Mutex<HashMap<String, PlayerData>>> = Arc::new(Mutex::new(HashMap::new()));
     let mut socket = Socket::bind("0.0.0.0:4242").unwrap();
     let packet_sender = socket.get_packet_sender();
     let event_receiver = socket.get_event_receiver();
     let _thread = thread::spawn(move || socket.start_polling());
 
     println!("starting server");
+
+    let clients_time_loop = Arc::clone(&clients);
+
+    thread::spawn(move || {
+        let ten_seconds = Duration::from_secs(10);
+        loop {
+            thread::sleep(ten_seconds);
+            let mut clients = clients_time_loop.lock().unwrap();
+            let mut clients_to_be_removed = Vec::new();
+
+            for client in clients.iter() {
+                if client.1.last_time_listened.elapsed().unwrap() >= ten_seconds {
+                    println!("Disconnecting user {}", client.0);
+                    clients_to_be_removed.push(client.0.clone());
+                }
+            }
+
+            for client in clients_to_be_removed {
+                clients.remove(&client);
+            }
+        }
+    });
+
+    let clients_addr = Arc::clone(&clients);
+
     loop{
         let result = event_receiver.recv();
 
@@ -20,17 +49,26 @@ fn main() {
                     SocketEvent::Packet(packet) => {
                         let endpoint = packet.addr();
                         let address = format!("{}:{}", endpoint.ip(), endpoint.port());
-                        match (&clients).get(&address){
-                            Some(_) => {},
+                        let mut clients = clients_addr.lock().unwrap();
+                        match (&mut clients).get_mut(&address){
+                            Some(client) => {
+                                client.last_time_listened = SystemTime::now();
+                            },
                             None => {
-                                println!("Player connected. port: {}", endpoint.port());
-                                (&mut clients).insert(address, endpoint);
+                                println!("Player connected: {}", address);
+                                (&mut clients).insert(
+                                    address, 
+                                    PlayerData { 
+                                        addr: endpoint, 
+                                        last_time_listened: SystemTime::now()
+                                    }
+                                );
                             }
                         }
                         let received_data = packet.payload();
                         for client in (&clients).values() {
-                            if client.ip() != endpoint.ip() || client.port() != endpoint.port(){
-                                let unreliable = Packet::unreliable(*client, received_data.to_vec());
+                            if client.addr.ip() != endpoint.ip() || client.addr.port() != endpoint.port(){
+                                let unreliable = Packet::unreliable(client.addr, received_data.to_vec());
                                 packet_sender.send(unreliable).unwrap();
                             }
                         }
